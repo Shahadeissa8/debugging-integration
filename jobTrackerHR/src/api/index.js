e5d61b7f8462d07fb2111c8b7e28b03c5bf2983a
Shahadeissa8 <shahadeissa98@gmail.com>
2025-03-11 16:57:43 +0300

    update

--- jobTrackerHR/src/api/index.js
+++ jobTrackerHR/src/api/index.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 // https://react-native-food-delivery-be.eapi.joincoded.com/api/category
 const instance = axios.create({
-  // baseURL: "http://localhost:5208",
-  baseURL: "http://192.168.2.235:5208",
-  // baseURL: "http://10.0.2.2:5208",
+  baseURL: "http://localhost:5208",
   // baseURL: "https://react-native-food-delivery-be.eapi.joincoded.com/",
 });
 
